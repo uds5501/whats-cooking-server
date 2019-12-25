27889daa14f1562e098ed746a430738f0f0a0f0a
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-12-25 13:56:28 +0530

    register result model to admin

--- ml/admin.py
+++ ml/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Result
 # Register your models here.
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ('description', 'prediction', 'confidence', 'is_correct', 'other_predcitions')
+
+admin.site.register(Result, ResultAdmin)
