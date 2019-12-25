a357d19a78d17fa4ef64ddc7df20bf5f78d84b0e
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-12-25 13:44:33 +0530

    create results model

--- ml/models.py
+++ ml/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Result(models.Model):
+
+    description = models.TextField()
+    prediction = models.CharField(max_length=50)
+    confidence = models.FloatField()
+    is_correct = models.BooleanField(null=True)
+    other_predictions = models.TextField()
